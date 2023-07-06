34a10b3ba2faba59e18e69b64ad75169d8b0be91
Anna Escolaro <annaescolaro@hotmail.com>
2023-07-06 14:37:57 -0300

    Requisito 3 finalizado

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import CardProduct from '../components/CardProduct';
 
 type ProductList = {
@@ -10,11 +11,17 @@ type ProductList = {
 
 function Index() {
   const [productList, setProductList] = useState<Array<ProductList>>([]);
+  const [typedProduct, setTypedProduct] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    setTypedProduct(event.target.value);
+  };
 
   return (
     <>
       <h2>Online Store Ts</h2>
-      <input type="text" />
+      <input type="text" onChange={ handleChange } />
       {productList.length > 0 ? (
         productList.map((product) => (
           <CardProduct
@@ -28,6 +35,9 @@ function Index() {
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h3>
       )}
+      <Link to="/cart" data-testid="shopping-cart-button">
+        <img src="./wireframes/cart.jpg" alt="carinho" />
+      </Link>
     </>
   );
 }
