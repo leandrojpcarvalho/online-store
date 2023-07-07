a2b0702e8b7afbada2fafdc4ee0c93cf4fe188e7
Luiz Henrique <seuemail@exemplo.br>
2023-07-07 17:15:53 -0300

    trabalho feito até agora

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,12 +1,42 @@
 import { useState } from 'react';
 
+interface Product {
+  id: string;
+  price: string;
+  thumbnail: string;
+  title: string;
+}
+
 function Cart() {
   const [isEmpty, setIsEmpty] = useState(true);
+  const [cart, setCart] = useState<Product[]>([]);
+
+  const addToCart = (product: Product) => {
+    const updatedCart = [...cart, product];
+    setCart(updatedCart);
+    localStorage.setItem('cart', JSON.stringify(updatedCart));
+    setIsEmpty(false);
+  };
 
   return (
-    isEmpty
-      ? <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
-      : <p>oi</p>
+    <div>
+      {isEmpty ? (
+        <p data-testid="shopping-cart-empty">Seu carrinho está vazio</p>
+      ) : (
+        <div>
+          {cart.map((product, index) => (
+            <div key={ index }>
+              <p data-testid="shopping-cart-product">{product.title}</p>
+              <p>
+                Price:
+                {' '}
+                {product.price}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
 
