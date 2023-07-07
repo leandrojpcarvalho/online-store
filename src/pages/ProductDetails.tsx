ccf69c06f5c8725d771b318e596255d4e4c88ed7
Anna Escolaro <annaescolaro@hotmail.com>
2023-07-07 14:36:27 -0300

    requisito 9 finalizado

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { getProductById } from '../services/api';
 import { Product } from '../types';
@@ -19,6 +19,8 @@ function ProductDetails() {
   const [product, setProduct] = useState(INITIAL_STATE);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const productId = GetIdProduct();
+  const { price, thumbnail, title } = product;
+
   useEffect(() => {
     const setProductState = async () => {
       if (!productId) throw new Error('novo Erro');
@@ -29,7 +31,18 @@ function ProductDetails() {
     setProductState();
   }, []);
 
-  const { price, thumbnail, title } = product;
+  const handleClick = () => {
+    const products = localStorage.getItem('products');
+    if (products === null) {
+      localStorage.setItem('products', JSON.stringify([
+        { title: product.title, price: product.price }]));
+    } else {
+      const products2 = JSON.parse(products);
+      products2.push({ title: product.title, price: product.price });
+      localStorage.setItem('products', JSON.stringify(products2));
+    }
+  };
+
   return (
     isLoading ? (
       <h2>Carregando...</h2>
@@ -42,6 +55,15 @@ function ProductDetails() {
           <h2 data-testid="product-detail-name">{ title }</h2>
           <h3 data-testid="product-detail-price">{ price }</h3>
         </div>
+        <button
+          data-testid="product-detail-add-to-cart"
+          onClick={ handleClick }
+        >
+          Adicionar ao carrinho
+        </button>
+        <Link to="/cart">
+          <button data-testid="shopping-cart-button">Ir para o carrinho</button>
+        </Link>
       </section>
     )
   );
